779c601c098af59405c3979c3eb6a5cd04c22de3
Fellow90 <poudelnabraaz99@gmail.com>
2023-05-14 08:44:10 +0545

    frontend updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('');
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const fetchNotes = async () => {
+    const response = await fetch('/api/notes');
+    const data = await response.json();
+    setNotes(data);
+  };
+
+  const addNote = async () => {
+    const response = await fetch('/api/notes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ content: newNote }),
+    });
+    const data = await response.json();
+    setNotes([...notes, data]);
+    setNewNote('');
+  };
+
+  const deleteNote = async (id) => {
+    await fetch(`/api/notes/${id}`, {
+      method: 'DELETE',
+    });
+    setNotes(notes.filter((note) => note.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <input
+          type="text"
+          value={newNote}
+          onChange={(e) => setNewNote(e.target.value)}
+        />
+        <button onClick={addNote}>Add Note</button>
+      </div>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            {note.content}
+            <button onClick={() => deleteNote(note.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
